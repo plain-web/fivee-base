3c9b248a669dd14ed299a4aa4396590f457d6ee6
jazzpoteto <jazzpoteto@gmail.com>
2015-11-05 23:47:22 +0900

    midfi/gulpfile | add bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-// use gulp
+// plugin
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
@@ -6,17 +6,21 @@ var haml = require('gulp-ruby-haml');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 var changed  = require('gulp-changed');
+//var spritesmith = require('gulp.spritesmith');
 
 /*========================================================================
   path
 ======================================================================== */ 
 var path = {
-  base:'./',
-  js    :'./js/*.js',
-  sass  :'./sass/**/*scss',
-  css  :'./css',
-  haml :'./*.haml',
-  html  :'./'
+  base: './',
+  js: './js/*.js',
+  sass: './sass/**/*scss',
+  css: './css',
+  haml: './*.haml',
+  html: './'
+  //sprite :'./img/sprite/*.png',
+  //spriteImg :'./img/',
+  //spriteScss :'./sass/'
 };
 /*========================================================================
   browser-sync
@@ -29,17 +33,36 @@ gulp.task('server', function() {
   });
 });
 /*========================================================================
+  css sprite
+======================================================================== */
+// gulp.task('sprite', function () {
+//   var spriteData = gulp.src(path.sprite)
+//     .pipe(plumber())
+//     .pipe(spritesmith({
+//       imgName: 'sprite.png',
+//       cssName: '_sprite.scss',
+//       cssFormat: 'scss'
+//     })
+//   );
+//   spriteData.img
+//     .pipe(gulp.dest(path.spriteImg));
+ 
+//   spriteData.css
+//     .pipe(gulp.dest(path.spriteScss));
+// });
+/*========================================================================
   sass
 ======================================================================== */
 gulp.task('sass', function() {
   gulp.src(path.sass)
-    .pipe(changed(path.css, { extension: '.css' }))
+    //.pipe(changed(path.css, { extension: '.css' }))
     .pipe(plumber())
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(gulp.dest(path.css))
     .pipe(browserSync.reload({stream:true}));
 });
+
 /*========================================================================
   haml
 ======================================================================== */
@@ -54,7 +77,6 @@ gulp.task('haml', function() {
     .pipe(gulp.dest(path.html))
     .pipe(browserSync.reload({stream:true}));
 });
- 
 /*========================================================================
   default (watch)
 ======================================================================== */
